db482073512daa39952d6e51c91c76165b261874
chi <chi_bui@outlook.com>
2016-10-09 17:56:42 +1100

    controller test

--- test/controllers/shop_controller_test.rb
+++ test/controllers/shop_controller_test.rb
@@ -4,6 +4,10 @@ class ShopControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get shop_index_url
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
